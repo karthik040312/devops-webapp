b033cf72800261661b618301758ab7d00539d05b
Reddygaru <reddygaru0412.com>
2025-04-24 11:18:27 +0530

    Add about page

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def home():
 	return "welcome to Devops web App!"
 
+@app.route("/about")
+def about():
+	return "This is About Page."
+
 if __name__=="__main__":
 	app.run(debug=True)
 
